fd18ed9d9889cb14bcf87fd6177c82012bfbffb2
Lee J. Rosenthal <leerosenthalj@gmail.com>
2019-07-15 23:01:10 -0700

    Fixed data input mistake.

--- radvel_setup_files/37124.py
+++ radvel_setup_files/37124.py
@@ -16,9 +16,9 @@ fitting_basis = 'per tc secosw sesinw k'
 bjd0 = 2450000.
 
 # load in data
-data = utils.read_from_csv('./setup_data/vst37124.csv')
-#data = cpsutils.io.loadcps('37124', hires_rk=True, hires_rj=True,
-#                           apf=True, ctslim=3000, binsize=0.5)
+#data = utils.read_from_csv('./setup_data/vst37124.csv')
+data = cpsutils.io.loadcps('37124', hires_rk=True, hires_rj=True,
+                           apf=True, ctslim=3000, binsize=0.5)
 if 'jd' in data.columns:
     data['time'] = data['jd']
 time_base = np.median(data['time'])
