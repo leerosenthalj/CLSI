822876a7103e1bd5a314c85aaea563df7dbe2254
Lee J. Rosenthal <leerosenthalj@gmail.com>
2019-07-30 14:45:03 -0400

    Added catalog scraper wrapper to repo.

---
+++ scrape_legacy.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import radvel
+
+from catalog_scrape import scrape
+
+starnames = [name for name in os.listdir('.')
+            if os.path.isdir(os.path.join('.', name))]
+
+system_props = scrape(starnames, mass_db_name='./legacy_specmatch_medians.csv')
