fc7cb5e381fcad2fe399f95a0f62f4f54920a6eb
Lee J. Rosenthal <leerosenthalj@gmail.com>
2019-10-22 14:17:11 -0700

    Reordered files.

---
+++ draw_eccentricities.py
@@ -0,0 +1,17 @@
+import numpy as np
+import scipy.optimize as op
+from scipy import stats
+import pandas as pd
+
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter, MaxNLocator
+import seaborn as sns
+import corner
+
+import astropy
+from astropy import stats as astrostats
+
+import emcee
+
+# 
