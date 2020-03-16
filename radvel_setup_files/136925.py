a1f87eb6aff7f159f3fa335fc92e2b7a146e1dd6
Lee J. Rosenthal <leerosenthalj@gmail.com>
2020-03-16 12:20:49 -0700

    Locking in 136925 setup file. Imperfect, but good enough.

--- radvel_setup_files/136925.py
+++ radvel_setup_files/136925.py
@@ -29,38 +29,54 @@ bjd0 = 2450000.
 stellar = dict(mstar=1.16, mstar_err=.12)
 
 # load in data
-data = utils.read_from_csv('setup_data/vst136925.csv')
-#data = cpsutils.io.loadcps('136925', hires_rk=True, hires_rj=True, lick=False, ctslim=3000, binsize=0.5)
+#data = utils.read_from_csv('setup_data/vst136925.csv')
+data = cpsutils.io.loadcps('136925', hires_rk=True, hires_rj=True, lick=False, ctslim=3000, binsize=0.5)
 data['time'] = data['jd']
 time_base = np.median(data['time'])
-#data['tel'] = data['tel'].str.decode('utf-8')
+data['tel'] = data['tel'].str.decode('utf-8')
 
 def initialize_params():
     '''
+    #Passing through the first datapoint.
     params = radvel.Parameters(1,basis='per tp e w k')
     params['per1'] = radvel.Parameter(value=3900.74)
-    params['tp1'] = radvel.Parameter(value=2453048.11) #8750
+    params['tp1'] = radvel.Parameter(value=2458400.11) #8750 3048.11
     params['k1'] = radvel.Parameter(value=22.27)
     params['e1'] = radvel.Parameter(value=0.708) #0.7
     params['w1'] = radvel.Parameter(value=1.952) #-1.0
     params['dvdt'] = radvel.Parameter(value=0.0045, vary=True)
     params['curv'] = radvel.Parameter(value=1.107e-06, vary=True)
     '''
+    #'''
+    #BEST OPTION AT THE MOMENT
+    #Not passing through the first datapoint. Try fitting proper periastron.
     params = radvel.Parameters(1,basis='per tc secosw sesinw k')
     params['per1'] = radvel.Parameter(value=4150)
-    params['tc1'] = radvel.Parameter(value=2454450.11) #8750
+    params['tc1'] = radvel.Parameter(value=2458750) #9000.11)#4450.11) #
     params['k1'] = radvel.Parameter(value=23.22)
     params['secosw1'] = radvel.Parameter(value=-0.33) #0.7
     params['sesinw1'] = radvel.Parameter(value=0.75) #-1.0
     params['dvdt'] = radvel.Parameter(value=0.0045, vary=True)
     params['curv'] = radvel.Parameter(value=1.107e-06, vary=True)
+    params['dvdt'].vary = True
+    params['curv'].vary = True
+    #'''
+    '''
+    #Try best results from mcmc.
+    params = radvel.Parameters(1,basis='per tc secosw sesinw k')
+    params['per1'] = radvel.Parameter(value=4206.5)
+    params['tc1'] = radvel.Parameter(value=2458400.)# 4300 is from mcmc, +per 8679.8
+    params['k1'] = radvel.Parameter(value=22.4)
+    params['secosw1'] = radvel.Parameter(value=-0.10) #0.7
+    params['sesinw1'] = radvel.Parameter(value=0.83) #-1.0
+    params['dvdt'] = radvel.Parameter(value=0.0045, vary=True)
+    params['curv'] = radvel.Parameter(value=1.107e-06, vary=True)
+    params['dvdt'].vary = True
+    params['curv'].vary = True
+    '''
     # Convert input orbital parameters into the fitting basis
     params = params.basis.to_any_basis(params,fitting_basis)
-
     return params
-    #params['per1'] = radvel.Parameter(value=13835.67)
-    #params['tp1'] = radvel.Parameter(value=2450593.5)
-	#params['k1'] = radvel.Parameter(value=755.3)
 
 # initialize the orbit parameters and the orbit model
 params = initialize_params()
