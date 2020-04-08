e4cdfbf51e940feadb9712fa92b439f11b3d5d7d
Lee J. Rosenthal <leerosenthalj@gmail.com>
2020-04-08 12:44:05 -0400

    More

--- radvel_setup_files/hip52942a.py
+++ radvel_setup_files/hip52942a.py
@@ -27,13 +27,9 @@ bjd0 = 2450000.
 stellar = dict(mstar=1.05, mstar_err=.06)
 
 # load in data
-'''
-data =
-'''
-#data = utils.read_from_csv('setup_data/HIP52942A.csv')
 data = cpsutils.io.loadcps('hip52942a', hires_rk=True, hires_rj=True,
                            ctslim=3000, binsize=0.5)
-data = data[data['obnm'] != 'rj81.412']  # low counts, only 30k and also poor seeing
+#data = data[data['obnm'] != 'rj81.412']  # low counts, only 30k and also poor seeing
 data['time'] = data['bjd']
 time_base = np.median(data['time'])
 data['tel'] = data['tel'].str.decode('utf-8')
